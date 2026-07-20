aafa2ecbf2816af9d33d73217682854b76fdb8ee
Http8php <2760332583@qq.com>
2026-07-20 19:40:00 +0800

    feat(template): 埃氏筛

--- template/数学/埃氏素数筛.cpp
+++ template/数学/埃氏素数筛.cpp
@@ -1,17 +1,18 @@
 /*
 const int N = 1e6 + 10;
-int vis[N];
+bool vis[N];
 vector<int>p;
 void init()
 {
-    for (int i = 2; i * i <= n; i++)
+    vis[0] = vis[1] = true;
+    for (int i = 2; i * i < N; i++)
     {
         if (!vis[i])
         {
             p.push_back(i);
-            for (int j = i * i; j <= n; j += i)
+            for (int j = i * i; j < N; j += i)
             {
-                if (!vis[j]) vis[j] = 1;
+                vis[j] = true;
             }
         }
     }
