f396bb47130fc39f9348688784ebefe9d7961daf
Http8php <2760332583@qq.com>
2026-07-27 10:58:04 +0800

    feat(template): 中心扩展

---
+++ template/字符串/中心扩展.cpp
@@ -0,0 +1,15 @@
+/*
+for (int i = 0; i < n; i++)
+{
+    int l = i - 1, r = i + 1;
+    while (l >= 0 && r < n && s[l] == s[r])
+    {
+        l--, r++;
+    }
+    l = i, r = i + 1;
+    while (l >= 0 && r < n && s[l] == s[r])
+    {
+        l--, r++;
+    }
+}
+*/
