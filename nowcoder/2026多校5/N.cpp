85d450703dd0572757e2ba1f52f9383ccf1e9e83
Http8php <2760332583@qq.com>
2026-08-01 22:29:59 +0800

    多校5N feat(nowcoder): sign in

---
+++ nowcoder/2026多校5/N.cpp
@@ -0,0 +1,26 @@
+/*
+思路：按题意输出
+
+时间：O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+void solve()
+{
+    cout << "HIT";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
