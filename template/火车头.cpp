4d7a4e6e069bed246276f422cfb70beb5f561f18
Http8php <2760332583@qq.com>
2026-08-10 20:04:31 +0800

    feat(template): 火车头

--- template/火车头.cpp
+++ template/火车头.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define eps 1e-6
 #define debug(x) cout << x << '\n';
 
+using ui = unsigned int;
 using ll = long long;
 using ull = unsigned long long;
 
@@ -20,11 +21,15 @@ char op[] = {'U', 'D', 'L', 'R'};
 int ddx[] = {-1, -1, -1, 0, 1, 1, 1, 0};
 int ddy[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 
+int month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+// "米"形拓展
 vector<pair<int, int> >p1 = {{0, 1}, {0, -1}};
 vector<pair<int, int> >p2 = {{-1, 0}, {1, 0}};
 vector<pair<int, int> >p3 = {{-1, -1}, {1, 1}};
 vector<pair<int, int> >p4 = {{1, -1}, {-1, 1}};
 
+// 随机数
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rand(int l, int r)
 {
@@ -32,7 +37,6 @@ int rand(int l, int r)
 }
 
 /*
-bool ok;
 cout << (ok ? "Yes" : "No") << '\n';
 
 cout << fixed << setprecision(6);
@@ -41,3 +45,27 @@ printf("? %d %d\n", i, j);
 printf("! %d\n", ans);
 fflush(stdout);
 */
+
+//判回文
+bool check(string s)
+{
+    int l = 0, r = s.length() - 1;
+    while (l < r)
+    {
+        if (s[l] != s[r]) return false;
+        l++, r--;
+    }
+    return true;
+}
+
+//判素数
+bool check(int x)
+{
+    if (x == 0 || x == 1) return false;
+    if (x == 2) return true;
+    for (int i = 2; i * i <= x; i++)
+    {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
