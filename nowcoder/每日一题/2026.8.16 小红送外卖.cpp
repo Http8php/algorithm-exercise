28e702a5807c3cb3a036b64c84c78d164c9ed2e6
Http8php <2760332583@qq.com>
2026-08-16 21:39:48 +0800

    feat(nowcoder): graphs

---
+++ nowcoder/每日一题/2026.8.16 小红送外卖.cpp
@@ -0,0 +1,85 @@
+/*
+标签：最短路
+
+思路：从1号点往返，就是最短路距离×2
+
+时间：O(nlogm)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int N = 1e5 + 10;
+const ll inf = 0x3f3f3f3f3f3f3f3f;
+struct node
+{
+    int v;
+    ll w;
+    friend bool operator<(node a, node b)
+    {
+        return a.w > b.w;
+    }
+};
+int n, m, q;
+vector<vector<node> >g(N);
+ll dis[N];
+int vis[N];
+void dij(int s)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        dis[i] = inf;
+        vis[i] = 0;
+    }
+    priority_queue<node>q;
+    dis[s] = 0;
+    q.push({s, 0});
+    while (!q.empty())
+    {
+        auto [u, d] = q.top();
+        q.pop();
+        if (vis[u]) continue;
+        vis[u] = 1;
+        for (auto [v, w] : g[u])
+        {
+            if (dis[v] > dis[u] + w)
+            {
+                dis[v] = dis[u] + w;
+                q.push({v, dis[v]});
+            }
+        }
+    }
+}
+void solve()
+{
+    cin >> n >> m >> q;
+    for (int i = 0, u, v; i < m; i++)
+    {
+        ll w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
+    }
+    dij(1);
+    ll ans = 0;
+    while (q--)
+    {
+        int a;
+        cin >> a;
+        ans += dis[a] * 2;
+    }
+    cout << ans;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
